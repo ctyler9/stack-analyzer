2783e6c70f47995ba44642d6a1aa875b98647355
Clay <ctyler9@me.com>
2023-01-21 13:46:01 -0500

    finished server, model training, reorg

---
+++ server/server.py
@@ -0,0 +1,55 @@
+import torch
+from PIL import Image
+import torchvision.transforms.functional as TF
+
+from flask import Flask
+app = Flask(__name__)
+
+
+classes_dict = {0: 'audemarspiguet',
+                1: 'patekphilippe',
+                2: 'breitling',
+                3: 'seiko',
+                4: 'jaegerlecoultre',
+                5: 'iwc',
+                6: 'gucci',
+                7: 'omega',
+                8: 'zenith',
+                9: 'panerai',
+                10: 'rolex',
+                11: 'cartier',
+                12: 'movado'}
+
+TRANSFORM = torch.jit.load("data/scripted_transform.pt")
+
+CLASSIFIER = torch.jit.load("data/scripted_model.pt")
+
+
+@app.route("/")
+def home():
+    return "Hello World"
+
+
+@app.route("/classify")
+def classify():
+    image = Image.open("./rolex.jpg")
+    img_tensor = TF.to_tensor(image)
+    x = TRANSFORM(img_tensor).unsqueeze(0)
+    prediction = CLASSIFIER(x)
+
+    return {
+        "brandPredictions": sorted(
+            list(
+                zip(
+                    classes_dict.values(),
+                    [round(x, 4) for x in map(float, prediction[0])]
+                )
+            ),
+            key=lambda p: p[1],
+            reverse=True
+        )
+    }
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
